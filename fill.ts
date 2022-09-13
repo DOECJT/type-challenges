feaa13f40b2224ac6429c7c59ea8728491b2c9c9
wuyawei <wuyawei@enn.cn>
2022-09-13 16:49:46 +0800

    practice

--- fill.ts
+++ fill.ts
@@ -1,15 +1,20 @@
-import { Expect, Equal } from './utils'
+import { Expect, Equal, NumberToArray } from './utils'
+
+type AddNum<T extends number, U extends number> = [...NumberToArray<T>, ...NumberToArray<U>]['length']
 
 type Fill<
   T extends unknown[],
   N,
   Start extends number = 0,
-  End extends number = T['length']
+  End extends number = T['length'],
+  U extends unknown[] = [],
 > = T extends [infer First, ...infer Rest]
-  ? Start extends End
-    ?
-    :
-  : T
+  ? Start extends End 
+    ? [...U, ...T]
+    : U['length'] extends Start
+      ? Fill<Rest, N, AddNum<Start, 1> & number, End, [...U, N]>
+      : Fill<Rest, N, Start, End, [...U, First]>
+  : U
 
 type exp = Fill<[1, 2, 3], 0> // expected to be [0, 0, 0]
 
